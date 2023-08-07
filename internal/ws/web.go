3913f71eb1e228d779cd55e41dabd76ef404e7db
connor-g <2417039231@qq.com>
2023-08-07 17:19:03 +0800

    修改ws 事件处理托盘、库位机制。

--- internal/ws/web.go
+++ internal/ws/web.go
@@ -66,29 +66,13 @@ func Web(_ context.Context) (err error) {
 }
 
 func Te(_ context.Context) (err error) {
-	// 新建一个定时任务对象
-	//// 根据cron表达式进行时间调度，cron可以精确到秒，大部分表达式格式也是从秒开始。
-	////crontab := cron.New()  默认从分开始进行时间调度
-	//crontab := cron.New(cron.WithSeconds()) //精确到秒
-	////定义定时器调用的任务函数
-	//task := func() {
-	//	fmt.Println("hello world", time.Now())
-	//}
-	////定时任务
-	//spec := "*/5 * * * * ?" //cron表达式，每五秒一次
-	//// 添加定时任务,
-	//crontab.AddFunc(spec, task)
-	//// 启动定时器
-	//crontab.Start()
-
 	time.Sleep(5 * time.Second)
 
 	room := "A511"
-	//total := 30
-	//ch := make(chan testData)
 	instackNos := make([]string, 0)
 
-	instackNos = append(instackNos, "030100041942",
+	instackNos = append(instackNos, "030100041200",
+		"030100041942",
 		"030100014441",
 		"030100041249",
 		"030100041244",
@@ -107,7 +91,6 @@ func Te(_ context.Context) (err error) {
 		"030100041231",
 		"030100041217",
 		"030100041229",
-		"030100041198",
 		"030100041230",
 		"030100041211",
 		"030100041209",
@@ -117,44 +100,46 @@ func Te(_ context.Context) (err error) {
 		"030100041214",
 		"030100041201",
 		"030100041204",
-		"030100041200")
+		"030100041198")
 
 	for i, no := range instackNos {
-		marshal, err := json.Marshal(testData{
+		marshal, err := json.Marshal(eventData{
 			StationNo:   "No.2[IN]",
 			CurrNum:     i + 1,
 			TotalNum:    30,
-			StationType: 1,
-			ContainerNo: no,
-			PosX:        1,
-			PosY:        1,
-			PosZ:        1})
+			ContainerNo: no})
 		if err != nil {
 			fmt.Println(err)
 		}
 		fmt.Printf("===============> %s\n", marshal)
-		Broadcast(room, string(marshal))
+
+		wsData := wsEventData{
+			EventType: "10",
+			Payload:   string(marshal),
+		}
+
+		bytes, err := json.Marshal(wsData)
+		if err != nil {
+			return err
+		}
+
+		Broadcast(room, string(bytes))
 		time.Sleep(time.Second)
 	}
-
 	revDatas := make([]view.InventoryContainerItemInfoView, 0)
-	_, err = app.GetOrm().Context.Raw(`select * from inventory_container_item_info_view 
-where area = 'PX01'
-and pos_x >=0
-order by pos_x ,pos_y ,pos_z`).QueryRows(&revDatas)
-	if err != nil {
+	if _, err = app.GetOrm().Context.Raw(`select * from inventory_container_item_info_view
+	where area = 'PX01'
+	and pos_x >=0
+	order by pos_x ,pos_y ,pos_z`).QueryRows(&revDatas); err != nil {
 		return err
 	}
 
 	// 入库操作
-	//revInstackNos := rev(instackNos)
-
 	for i, no := range revDatas {
-		marshal, err := json.Marshal(testData{
+		marshal, err := json.Marshal(eventData{
 			StationNo:   "No.2[IN]",
 			CurrNum:     i + 1,
 			TotalNum:    30,
-			StationType: 2, // 入库
 			ContainerNo: no.ContainerNo,
 			PosX:        no.PosX,
 			PosY:        no.PosY,
@@ -165,69 +150,38 @@ order by pos_x ,pos_y ,pos_z`).QueryRows(&revDatas)
 			fmt.Println(err)
 		}
 		fmt.Printf("===============> %s\n", marshal)
-		Broadcast(room, string(marshal))
+		// 发送消息到 前端ws
+
+		wsData := wsEventData{
+			EventType: "20",
+			Payload:   string(marshal),
+		}
+
+		bytes, err := json.Marshal(wsData)
+		Broadcast(room, string(bytes))
 		time.Sleep(time.Second)
 	}
-
-	//for c := 0; c < total; c++ {
-	//
-	//	go func(child int) {
-	//		// 向ch 信道发送信息
-	//		ch <- datas[child]
-	//		time.Sleep(time.Second)
-	//	}(c)
-	//}
-	//// 遍历缓存数，接受所有信道的中数据
-	//for total > 0 {
-	//	// 从ch 信道中获取数据，没有数据则阻塞等待
-	//	c := <-ch
-	//	// 缓存数递减
-	//	total--
-	//	marshal, err := json.Marshal(c)
-	//	if err != nil {
-	//		fmt.Println(err)
-	//	}
-	//	fmt.Printf("===============> %s\n", marshal)
-	//	Broadcast(room, string(marshal))
-	//}
-
 	return err
-
-	//time5 := time.After(time.Second * 5)
-	//quit := make(chan struct{})
-	//
-	//go func() {
-	//	for {
-	//		select {
-	//		case <-time5:
-	//			quit <- struct{}{}
-	//		case <-time.After(time.Second * 1):
-	//			marshal, err := json.Marshal(data)
-	//			if err != nil {
-	//				fmt.Println(err)
-	//			}
-	//			fmt.Printf("===============> %s\n", marshal)
-	//			Broadcast(room, string(marshal))
-	//			data.CurrNum++
-	//		}
-	//	}
-	//}()
-	//<-quit
-	//fmt.Printf("done...")
-	//return nil
 }
 
-type testData struct {
+// 发送前台ws json数据
+type wsEventData struct {
+	EventType string `json:"eventType"`
+	Payload   string `json:"payload"`
+}
+
+// 托盘、库位数据
+type eventData struct {
 	StationNo   string `json:"stationNo"`
 	CurrNum     int    `json:"currNum"`
 	TotalNum    int    `json:"totalNum"`
-	StationType int    `json:"stationType"`
 	ContainerNo string `json:"containerNo"`
 	PosX        int    `json:"posX"`
 	PosY        int    `json:"posY"`
 	PosZ        int    `json:"posZ"`
 }
 
+// 数组反转
 func rev(slice []string) []string {
 	fmt.Println(slice)
 	for i, j := 0, len(slice)-1; i < j; i, j = i+1, j-1 {
